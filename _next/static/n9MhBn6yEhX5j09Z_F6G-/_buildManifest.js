6e0c717381c54559d83542787ba55bba6f87c10f
alejandrojortiz <98439560+alejandrojortiz@users.noreply.github.com>
2023-01-27 08:51:07 +0000

    Deploying to gh-pages from @ alejandrojortiz/alejandrojortiz.github.io@b9d8104a28b3a5c8c9d004fe77d20b2bc83188b6 🚀

--- _next/static/n9MhBn6yEhX5j09Z_F6G-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-d0069c9baed76169.js","static/chunks/275-2921d01a6fd4bf06.js","static/chunks/pages/index-0c29d5486699b58b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/qzviVC58dtrqLc07Hs1eE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-d0069c9baed76169.js","static/chunks/275-a491b1b5ffaa9c20.js","static/chunks/pages/index-cf3ccd8817bdeb8c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
