94d3043b86671783435301a8567e658ede2480a7
alejandrojortiz <98439560+alejandrojortiz@users.noreply.github.com>
2023-11-14 21:05:24 +0000

    Deploying to gh-pages from @ alejandrojortiz/alejandrojortiz.github.io@22bc539f23133d5951a7a47babe4ca4ffac78dbb 🚀

---
+++ _next/static/VPGHc5j_lsbiXU67BLROd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-d0069c9baed76169.js","static/chunks/275-a491b1b5ffaa9c20.js","static/chunks/pages/index-d90f72eb3308af42.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
